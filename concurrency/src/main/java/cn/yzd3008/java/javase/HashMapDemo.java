42be5ceefccdce471714d999fa8bc91b505a3fb5
Ted Duan <yzd3008@icloud.com>
2021-10-19 20:08:18 +0800

    Commit HashMap demo

---
+++ concurrency/src/main/java/cn/yzd3008/java/javase/HashMapDemo.java
@@ -0,0 +1,32 @@
+package cn.yzd3008.java.javase;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Slf4j
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+
+        new HashMap(4);
+        Map<String, String> map = new HashMap<>();
+
+        // put
+        log.info("=== put demo");
+        int N = 10000;
+        int i = 0;
+        while (i <= N) {
+            i++;
+            map.put(String.valueOf(i), String.valueOf(i));
+        }
+
+        log.info("=== end put demo");
+
+
+    }
+
+
+}
